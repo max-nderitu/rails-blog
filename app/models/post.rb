0ed192f62b59386eb017f273275520e09b37f9d8
Maxwel Nderitu <m.nderitu@carepay.com>
2024-08-07 09:58:20 +0300

    adding post description field

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
-  has_rich_text :content
+  has_rich_text :content 
+  has_rich_text :description 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_04_152611) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_07_064645) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_04_152611) do
     t.string "poster"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
